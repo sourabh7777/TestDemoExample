9c376c5869796d97f04ecfbab87a88012e279736
sourabh7777 <sourabhbehera916@gmail.com>
2023-05-15 10:21:16 +0530

    added readme

---
+++ src/demo/jenkins/JenkinsDemo.java
@@ -0,0 +1,41 @@
+package demo.jenkins;
+
+import org.testng.annotations.Test;
+
+public class JenkinsDemo {
+	
+	@Test
+	public void TestOne() {
+		System.out.println("Test Case one Passed");
+	}
+	
+	@Test
+	public void TestTwo() {
+		System.out.println("Test Case Two Passed");
+	}
+	
+	@Test
+	public void TestThree() {
+		System.out.println("test case Three Passed");
+		System.out.println("Welcome to jenkins world ....!");
+		System.out.println("Jenkins are used to CI/CD");
+	}
+	
+	@Test
+	public void TestFour() {
+		System.out.println("Test Case Four Passed");
+	}
+	
+	@Test
+	public void TestFive() {
+		System.out.println("Test Case Five Passed");
+	}
+	
+	@Test
+	public void TestSix() {
+		System.out.println("test case Six Passed");
+		System.out.println("Welcome to jenkins world ....!");
+		System.out.println("Jenkins are used to CI/CD");
+	}
+
+}
